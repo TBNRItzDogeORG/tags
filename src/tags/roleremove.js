920c6562fa4052d9d7290cd12ce6adfd8d1d18c1
doge <shadowphoenixcoc@gmail.com>
2020-06-04 11:23:29 -0400

    updateo

--- src/tags/roleremove.js
+++ src/tags/roleremove.js
@@ -7,8 +7,8 @@ module.exports = {
 		const rolename = ctx.value;
 		if (!rolename) throw 'You need to provide a role ID to add separated by a colon. Example: {roleadd:1111111111}';
 		const role = regex.role.test(rolename)
-			? ctx.guild.roles.get(rolename)
-			: ctx.guild.roles.find(rol => rol.name.toLowerCase() === rolename.toLowerCase());
+			? ctx.guild.roles.cache.get(rolename)
+			: ctx.guild.roles.cache.find(rol => rol.name.toLowerCase() === rolename.toLowerCase());
 		if (!role) throw 'Invalid role. Please provide a valid role name or ID.';
 		ctx.member.roles.remove(role);
 		return '';
