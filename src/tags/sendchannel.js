1014d582697a72e69134640666345f8ad7badbc9
doge <shadowphoenixcoc@gmail.com>
2020-06-09 13:13:59 -0400

    add

---
+++ src/tags/sendchannel.js
@@ -0,0 +1,23 @@
+const { regex } = require('../util/utils.js');
+module.exports = {
+  name: "sendchannel",
+  aliases: ["channelsend"],
+  run: (ctx) => {
+    const [id, message] = ctx.value;
+    if (!id || !message)
+      throw "You must provide a valid channel ID and content.";
+    let channel;
+    if (regex.channel.test(id)) {
+      channel = ctx.guild.channels.cache.get(id);
+    } else {
+      channel = ctx.guild.channels.cache.find((chan) => chan.name === id);
+    }
+    if (!channel || channel.type === "voice")
+      throw "An invalid channel or channel type was specified.";
+    try{
+      return channel.send(message);
+    }catch(err){
+      throw "I do not have permissions to send messages to that channel.";
+    }
+  },
+};
